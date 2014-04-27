11f0169653cf6fab0d9b9bccb11bb5414b9aaaa5
saran-sasidharan <saran.s.dhar@gmail.com>
2014-04-27 19:13:00 +0530

    Devectorized Julia

--- julia_advect.jl
+++ julia_advect.jl
@@ -1,7 +1,7 @@
 T1 = time()
 H,L = 1000.0,1000.0
 
-jmax,imax = 302,302
+jmax,imax = 12,12
 
 dx = L/(imax-2)
 dy = H/(jmax-2)
@@ -19,19 +19,22 @@ t_bottom = 0.0
 t_right = 0.0
 
 t = zeros(jmax,imax)
-t[:,1] = t_left
-t[end,:] = t_bottom
-t[:,end] = t_right
-t[1,:] = t_top
-
+for i in 1:jmax
+	t[i,1] = t_left
+	t[end,i] = t_bottom
+	t[i,end] = t_right
+	t[1,i] = t_top
+end
 dt = (0.1*dx)/u
 
 t_x = zeros(jmax-2,imax-1)
 t_y = zeros(jmax-1,imax-2)
-t_x[:,1] = t[2:end-1,1]
-t_x[:,end] = t[2:end-1,end]
-t_y[1,:] = t[1,2:end-1]
-t_y[end,:] = t[end,2:end-1]
+for i in 1:jmax-2
+	t_x[i,1] = t[i+1,1]	
+	t_x[i,end] = t[i+1,end]
+	t_y[1,i] = t[1,i+1]
+	t_y[end,i] = t[end,i+1]
+end
 
 mx = rho*u
 my = rho*v
@@ -40,16 +43,28 @@ wpx = ones(jmax-2,imax-3)
 wpy = ones(jmax-3,imax-2)
 
 constant_a = dt/(rho*cp*dx*dy)
-iterations = 0
+q_adv = zeros(jmax-2,imax-2)
 
-while iterations < maxiter
-    iterations += 1
-    t_x[:,2:end-1] = wpx.*t[2:end-1,2:end-2]
-    t_y[2:end-1,:] = wpy.*t[3:end-1,2:end-1]
-    adv_x = mx*cp*dy*t_x
-    adv_y = my*cp*dx*t_y
-    q_adv = (adv_x[:,2:end]-adv_x[:,1:end-1]) + (adv_y[1:end-1,:]-adv_y[2:end,:])
-    t[2:end-1,2:end-1] = t[2:end-1,2:end-1] - constant_a*q_adv
+for iter in 1:maxiter
+        for j in 1:jmax-2
+		for i in 1:imax-3
+      			t_x[j,i+1] = wpx[j,i]*t[j+1,i+1]
+		end
+	end
+        for j in 1:jmax-3
+		for i in 1:imax-2
+      			t_y[j+1,i] = wpy[j,i]*t[j+2,i+1]
+		end
+	end 
+    	adv_x = mx*cp*dy*t_x
+    	adv_y = my*cp*dx*t_y
+	for j in 1:jmax-2
+		for i in 1:imax-2
+    			q_adv[j,i] = (adv_x[j,i+1]-adv_x[j,i]) + (adv_y[j,i]-adv_y[j+1,i])
+    			t[j+1,i+1] = t[j+1,i+1] - constant_a*q_adv[j,i]
+		end
+	end
 end
 T2 = time()
 println(T2-T1)
+println(t)
